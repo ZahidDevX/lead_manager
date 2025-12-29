62ab9de45f10952e47352c58132f82d4af1ed241
mdhamim03 <hamimmiagemd1@gmail.com>
2025-12-29 17:24:01 +0600

    Setup primevue

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Services\Core\SidebarMenuService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
@@ -41,10 +42,10 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'name' => config('app.name'),
-            'quote' => ['message' => trim($message), 'author' => trim($author)],
             'auth' => [
                 'user' => $request->user(),
             ],
+            'menu'=> app(SidebarMenuService::class)->build()
         ];
     }
 }
---
+++ resources/js/layouts/AppFooter.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <div class="layout-footer">
+        Developed & Maintained by
+        <a href="https://mustamirun.agency/" target="_blank" rel="mustamirun"
+            class="text-primary font-bold hover:underline">Mustamirun</a>
+    </div>
+</template>
